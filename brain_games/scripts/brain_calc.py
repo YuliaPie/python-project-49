330fa25cd0b1e1e66931a9e59f74946d61285569
Yulia <jilly-2005@yandex.ru>
2024-02-07 07:29:08 +0300

    add asker, brain_calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import random
+from brain_games import main
+from asker import ask_three_times
+
+name = main()
+print('What is the result of the expression?')
+def calc_randoms():
+    first = random.randint(1, 100)
+    second = random.randint(1, 100)
+    operation = random.choices("+-*")
+    if operation[0] == "+":
+        correct_answer = first + second
+    if operation[0] == "-":
+        correct_answer = first - second
+    if operation[0] == "*":
+        correct_answer = first * second
+    question = f"{first} {operation[0]} {second}"
+    return [question, correct_answer]
+calc_randoms = [calc_randoms(),calc_randoms(),calc_randoms()]
+ask_three_times(calc_randoms, name)
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
 import random
-from brain_games.scripts.brain_games import main
+from brain_games import main
+from asker import ask_three_times
 
 name = main()
 print('Answer "yes" if the number is even, otherwise answer "no".')
-correct_answer_counter = 0
-while correct_answer_counter < 3:
+
+
+def even_randoms():
     number = random.randint(1, 100)
-    print(f"Question: {number}")
-    answer = input("Your answer: ")
     if number % 2 == 0:
         correct_answer = "yes"
     if number % 2 != 0:
         correct_answer = "no"
-    if answer == correct_answer:
-        correct_answer_counter += 1
-        print("Correct!")
-        if correct_answer_counter == 3:
-            print(f"Congratulations, {str(name)}!")
-            exit()
+    question = f"{number}"
+    return [question, correct_answer]
 
-    else:
-        print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}.")
-        print(f"Let's try again, {str(name)}!")
-        correct_answer_counter = 0
-        exit()
+
+ask_three_times([even_randoms(), even_randoms(), even_randoms()], name)
