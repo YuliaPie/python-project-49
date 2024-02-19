6f11f2ce873317b583467e959693db190329d498
Yulia <jilly-2005@yandex.ru>
2024-02-19 11:58:01 +0300

    modified brain_games/games/even.py added asciinema files

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,10 +4,11 @@ import random
 TASK = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
+def is_even(number):
+    return False if number % 2 else True
+
+
 def get_question_and_answer():
     number = random.randint(1, 100)
-
-    def is_even(number):
-        return False if number % 2 else True
     answer = "yes" if is_even(number) else "no"
     return str(number), answer
